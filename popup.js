fa3c3ec26839974142f125fd3745d0a4726a41cb
JORDI\jnava <jnavarroescrivas2016@gmail.com>
2022-03-09 17:13:02 +0100

    modificar el js,  crear valoracion.html y modificacion de contenido de las otras. By Alex i Jordi

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('button');
+const button = document.querySelector('.button');
 const popup = document.querySelector('.popup-wrapper');
 const close = document.querySelector('.popup-close');
  
